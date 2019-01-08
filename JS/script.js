25fa5171722a0c46f06c3110760a914b7151e447
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2019-01-08 11:20:17 -0600

    adding JQuery

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+$(function() {
+    $('.main-title').hide().delay(500).fadeIn(2000)
+
+    $('.recipes-title').hide().delay(500).fadeIn(2000)
+
+    $(window).scroll( function() {
+        $('.recipes-title')
+    })
+})
+
