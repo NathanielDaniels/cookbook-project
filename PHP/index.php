7c0234670579266c290ffba399b8f9a68b0d8f4b
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2019-01-21 10:07:01 -0600

    Cleaned up CSS. Working on PhP for Form

--- PHP/index.php
+++ PHP/index.php
@@ -1,6 +1,13 @@
-
-
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Simnone's Recipe Box</title>
+</head>
+<body>
+  <?php
   if($_POST['form-submit'] == "submit")
   {
     $errorMessage = "";
@@ -39,4 +46,17 @@
 
 
 ?>
+
+<!-- Trying to get this to work. below code may not be needed -->
+<?php
+  // The global $_POST variable allows you to access the data sent with the POST method by name
+  // To access the data sent with the GET method, you can use $_GET
+  $recipeName = htmlspecialchars($_POST['recipe-name']);
+  $ingredientList  = htmlspecialchars($_POST['ingredient-list']);
+
+  echo  $recipeName, ' ', $ingredientList;
+?>
+
+</body>
+</html>
 
