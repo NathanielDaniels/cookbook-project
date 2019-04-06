be7ad8449ada642ac6461fa888c3fced7fdf69c2
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2019-04-06 17:20:33 -0500

    got everything setup ready for PHP form

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,17 @@
-// $(function() {
-//     $('.main-title').hide().delay(500).fadeIn(2000)
+$(function() {
+  $(".main-title")
+    .hide()
+    .delay(100)
+    .fadeIn(2000);
 
-//     $('.recipes-title').hide().delay(500).fadeIn(2000)
+  // $(window).scroll(function() {
+  //   $(".recipes-title")
+  //     .hide()
+  //     .delay(500)
+  //     .fadeIn(2000);
+  // });
+});
 
-//     $(window).scroll( function() {
-//         $('.recipes-title')
-//     })
-// })
+const formRecipes = document.getElementById("formRecipes");
+const recipesList = document.querySelector("#recipesList");
+const form = document.getElementsByClassName("recipe-name");
