d872e37d7275b29d239412664a19ff9b912b038f
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2020-05-12 00:16:46 -0500

    added an ok btn animation

--- JS/script.js
+++ JS/script.js
@@ -4,7 +4,10 @@ const form = document.getElementsByClassName("recipe-name");
 
 $(function () {
   $(".main-title").hide().delay(100).fadeIn(2000);
-  $("#btn").hide().delay(200).fadeIn(1000);
+  $("#btn").hide().delay(100).fadeIn(2000).animate({
+    bottom: "-55px",
+    opacity: 0.9,
+  });
 
   // $(window).scroll(function() {
   //   $(".recipes-title")
