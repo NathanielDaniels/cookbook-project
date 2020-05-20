e3c3b9af7ef78ab5a8894fb562f8396fe0f9656e
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2020-05-20 17:33:12 -0500

    Top Recipe scroll working (amature)

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,7 @@
 const formRecipes = document.getElementById("formRecipes");
 const recipesList = document.querySelector("#recipesList");
 const form = document.getElementsByClassName("recipe-name");
+
 const prevArrow = document.querySelector(".prev-arrow");
 const nextArrow = document.querySelector(".next-arrow");
 const recipes = document.querySelectorAll(".grid-item");
@@ -13,17 +14,39 @@ $(function () {
   });
 });
 
+let counter = 0;
+console.log(counter);
 prevArrow.addEventListener("click", () => {
-  for (let i = 0; i < recipes.length; i++) {
-    let moveLeft = (parseInt(recipes[i].style.left, 10) || 0) - 300;
-    recipes[i].style.position = "relative";
-    recipes[i].style.left = moveLeft + "px";
+  counter--;
+  console.log(counter);
+  if (counter >= -3 && counter <= 3) {
+    counter--;
+    for (let i = 0; i < recipes.length; i++) {
+      let moveLeft = (parseInt(recipes[i].style.left, 10) || 0) - 320;
+      recipes[i].style.position = "relative";
+      recipes[i].style.left = moveLeft + "px";
+    }
+  } else if (counter < -3) {
+    counter++;
+  } else {
+    // recipes.disabled = true;
+    prevArrow.disabled = true;
   }
 });
 nextArrow.addEventListener("click", () => {
-  for (let i = 0; i < recipes.length; i++) {
-    let moveRight = (parseInt(recipes[i].style.left, 10) || 0) + 300;
-    recipes[i].style.position = "relative";
-    recipes[i].style.left = moveRight + "px";
+  counter++;
+  console.log(counter);
+  if (counter <= 3 && counter >= -3) {
+    counter++;
+    for (let i = 0; i < recipes.length; i++) {
+      let moveRight = (parseInt(recipes[i].style.left, 10) || 0) + 320;
+      recipes[i].style.position = "relative";
+      recipes[i].style.left = moveRight + "px";
+    }
+  } else if (counter > 3) {
+    counter--;
+  } else {
+    // recipes.disabled = true;
+    nextArrow.disabled = true;
   }
 });
