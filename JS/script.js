d00118baeff062ba212362652c096a6b9a703007
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2020-06-29 14:08:21 -0500

    added node.js

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,5 @@
+import recipesArr from "./recipes";
+
 const prevArrow = document.querySelector(".prev-arrow");
 const nextArrow = document.querySelector(".next-arrow");
 const scrollRecipes = document.querySelectorAll(".scroll");
@@ -47,7 +49,7 @@ let overlayModal = document.querySelector(".overlayModal");
 
 console.log(allRecipeItems);
 
-//! Recipe Item Click Func. (ONLY TOP RECIPES FOR NOW)
+//! Recipe Item Click (All Recipes (Top + Main))
 function addEventListenerList(list) {
   for (let i = 0; i < allRecipeItems.length; i++) {
     // list[i].addEventListener("click", () => {
@@ -153,102 +155,278 @@ function addEventListenerList(list) {
 addEventListenerList(hoverText);
 
 //! Add Dynamic Content to modul overlay
-const recipesArr = [
-  {
-    location: "Top Recipes",
-    title: "Taco Soup",
-    info: "Talk about taco Soup",
-    ingredients: "&#8226 Taco Soup <br> &#8226 Taco Soup <br>",
-    instructions: "instructions here instructions here instructions here",
-  },
-  {
-    location: "Top Recipes",
-    title: "Carrot Veggie Dish",
-    info: "Talk about Carrot Veggie Dish",
-    ingredients: `<ul>
-        <li>&#8226 Carrot Veggie Dish</li>
-        <li>&#8226 Carrot Veggie Dish</li>
-        <li>&#8226 Carrot Veggie Dish</li>
-        <li>&#8226 Carrot Veggie Dish</li>
-        <li>&#8226 Carrot Veggie Dish</li>
-      </ul>`,
-    instructions: `<ul>
-      <li>instructions here instructions here instructions here</li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-    </ul>`,
-  },
-  {
-    location: "Top Recipes",
-    title: "Cast Iron Steak",
-    info: "Talk about cast iron steak",
-    ingredients: "&#8226 ",
-    instructions: `<ul>
-      <li>instructions here instructions here instructions here</li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-    </ul>`,
-  },
-  {
-    location: "Top Recipes",
-    title: "Taco Cups",
-    info: "Talk about Taco Cups",
-    ingredients: "&#8226 ",
-    instructions: `<ul>
-      <li>instructions here instructions here instructions here</li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-    </ul>`,
-  },
-  {
-    location: "Top Recipes",
-    title: "Pan fried COD",
-    info: "Talk about Pan Frying COD",
-    ingredients: "&#8226 Pan fried COD",
-    instructions: `<ul>
-      <li>instructions here instructions here instructions here</li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-    </ul>`,
-  },
-  {
-    location: "Top Recipes",
-    title: "Blu+Bri Burgers",
-    info: "Talk about Blu + Bri Burgers",
-    ingredients: "&#8226 Blu + Bri Burgers",
-    instructions: `<ul>
-      <li>instructions here instructions here instructions here</li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-    </ul>`,
-  },
-  {
-    location: "Main Recipes",
-    title: "Main Recipe Section Test",
-    info: "Test Test Test",
-    ingredients: "&#8226 Test Test",
-    instructions: `<ul>
-      <li>instructions here instructions here instructions here</li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-    </ul>`,
-  },
-];
 
-//* ============================================
+// const topRecipeArr = [
+//   {
+//     // location: "Top Recipes",
+//     title: "Taco Soup",
+//     info: "Talk about taco Soup",
+//     ingredients: "&#8226 Taco Soup <br> &#8226 Taco Soup <br>",
+//     instructions: "instructions here instructions here instructions here",
+//   },
+//   {
+//     // location: "Top Recipes",
+//     title: "Carrot Veggie Dish",
+//     info: "Talk about Carrot Veggie Dish",
+//     ingredients: `<ul>
+//         <li>&#8226 Carrot Veggie Dish</li>
+//         <li>&#8226 Carrot Veggie Dish</li>
+//         <li>&#8226 Carrot Veggie Dish</li>
+//         <li>&#8226 Carrot Veggie Dish</li>
+//         <li>&#8226 Carrot Veggie Dish</li>
+//       </ul>`,
+//     instructions: `<ul>
+//       <li>instructions here instructions here instructions here</li>
+//       <li></li>
+//       <li></li>
+//       <li></li>
+//       <li></li>
+//     </ul>`,
+//   },
+//   {
+//     // location: "Top Recipes",
+//     title: "Cast Iron Steak",
+//     info: "Talk about cast iron steak",
+//     ingredients: "&#8226 ",
+//     instructions: `<ul>
+//       <li>instructions here instructions here instructions here</li>
+//       <li></li>
+//       <li></li>
+//       <li></li>
+//       <li></li>
+//     </ul>`,
+//   },
+//   {
+//     // location: "Top Recipes",
+//     title: "Taco Cups",
+//     info: "Talk about Taco Cups",
+//     ingredients: "&#8226 ",
+//     instructions: `<ul>
+//       <li>instructions here instructions here instructions here</li>
+//       <li></li>
+//       <li></li>
+//       <li></li>
+//       <li></li>
+//     </ul>`,
+//   },
+//   {
+//     // location: "Top Recipes",
+//     title: "Pan fried COD",
+//     info: "Talk about Pan Frying COD",
+//     ingredients: "&#8226 Pan fried COD",
+//     instructions: `<ul>
+//       <li>instructions here instructions here instructions here</li>
+//       <li></li>
+//       <li></li>
+//       <li></li>
+//       <li></li>
+//     </ul>`,
+//   },
+//   {
+//     // location: "Top Recipes",
+//     title: "Blu+Bri Burgers",
+//     info: "Talk about Blu + Bri Burgers",
+//     ingredients: "&#8226 Blu + Bri Burgers",
+//     instructions: `<ul>
+//       <li>instructions here instructions here instructions here</li>
+//       <li></li>
+//       <li></li>
+//       <li></li>
+//       <li></li>
+//     </ul>`,
+//   },
+// ];
+
+// const mainRecipeArr = [
+//   {
+//     // location: "Main Recipes",
+//     title: "Main Recipe Section Test",
+//     info: "Test Test Test",
+//     ingredients: "&#8226 Test Test",
+//     instructions: `<ul>
+//       <li>instructions here instructions here instructions here</li>
+//       <li></li>
+//       <li></li>
+//       <li></li>
+//       <li></li>
+//     </ul>`,
+//   },
+//   {
+//     // location: "Main Recipes",
+//     title: "Main Recipe Section Test",
+//     info: "Test Test Test",
+//     ingredients: "&#8226 Test Test",
+//     instructions: `<ul>
+//       <li>instructions here instructions here instructions here</li>
+//       <li></li>
+//       <li></li>
+//       <li></li>
+//       <li></li>
+//     </ul>`,
+//   },
+//   {
+//     // location: "Main Recipes",
+//     title: "Main Recipe Section Test",
+//     info: "Test Test Test",
+//     ingredients: "&#8226 Test Test",
+//     instructions: `<ul>
+//       <li>instructions here instructions here instructions here</li>
+//       <li></li>
+//       <li></li>
+//       <li></li>
+//       <li></li>
+//     </ul>`,
+//   },
+//   {
+//     // location: "Main Recipes",
+//     title: "Main Recipe Section Test",
+//     info: "Test Test Test",
+//     ingredients: "&#8226 Test Test",
+//     instructions: `<ul>
+//       <li>instructions here instructions here instructions here</li>
+//       <li></li>
+//       <li></li>
+//       <li></li>
+//       <li></li>
+//     </ul>`,
+//   },
+//   {
+//     // location: "Main Recipes",
+//     title: "Main Recipe Section Test",
+//     info: "Test Test Test",
+//     ingredients: "&#8226 Test Test",
+//     instructions: `<ul>
+//       <li>instructions here instructions here instructions here</li>
+//       <li></li>
+//       <li></li>
+//       <li></li>
+//       <li></li>
+//     </ul>`,
+//   },
+//   {
+//     // location: "Main Recipes",
+//     title: "Main Recipe Section Test",
+//     info: "Test Test Test",
+//     ingredients: "&#8226 Test Test",
+//     instructions: `<ul>
+//       <li>instructions here instructions here instructions here</li>
+//       <li></li>
+//       <li></li>
+//       <li></li>
+//       <li></li>
+//     </ul>`,
+//   },
+//   {
+//     // location: "Main Recipes",
+//     title: "Main Recipe Section Test",
+//     info: "Test Test Test",
+//     ingredients: "&#8226 Test Test",
+//     instructions: `<ul>
+//       <li>instructions here instructions here instructions here</li>
+//       <li></li>
+//       <li></li>
+//       <li></li>
+//       <li></li>
+//     </ul>`,
+//   },
+//   {
+//     // location: "Main Recipes",
+//     title: "Main Recipe Section Test",
+//     info: "Test Test Test",
+//     ingredients: "&#8226 Test Test",
+//     instructions: `<ul>
+//       <li>instructions here instructions here instructions here</li>
+//       <li></li>
+//       <li></li>
+//       <li></li>
+//       <li></li>
+//     </ul>`,
+//   },
+//   {
+//     // location: "Main Recipes",
+//     title: "Main Recipe Section Test",
+//     info: "Test Test Test",
+//     ingredients: "&#8226 Test Test",
+//     instructions: `<ul>
+//       <li>instructions here instructions here instructions here</li>
+//       <li></li>
+//       <li></li>
+//       <li></li>
+//       <li></li>
+//     </ul>`,
+//   },
+//   {
+//     // location: "Main Recipes",
+//     title: "Main Recipe Section Test",
+//     info: "Test Test Test",
+//     ingredients: "&#8226 Test Test",
+//     instructions: `<ul>
+//       <li>instructions here instructions here instructions here</li>
+//       <li></li>
+//       <li></li>
+//       <li></li>
+//       <li></li>
+//     </ul>`,
+//   },
+//   {
+//     // location: "Main Recipes",
+//     title: "Main Recipe Section Test",
+//     info: "Test Test Test",
+//     ingredients: "&#8226 Test Test",
+//     instructions: `<ul>
+//       <li>instructions here instructions here instructions here</li>
+//       <li></li>
+//       <li></li>
+//       <li></li>
+//       <li></li>
+//     </ul>`,
+//   },
+//   {
+//     // location: "Main Recipes",
+//     title: "Main Recipe Section Test",
+//     info: "Test Test Test",
+//     ingredients: "&#8226 Test Test",
+//     instructions: `<ul>
+//       <li>instructions here instructions here instructions here</li>
+//       <li></li>
+//       <li></li>
+//       <li></li>
+//       <li></li>
+//     </ul>`,
+//   },
+//   {
+//     // location: "Main Recipes",
+//     title: "Main Recipe Section Test",
+//     info: "Test Test Test",
+//     ingredients: "&#8226 Test Test",
+//     instructions: `<ul>
+//       <li>instructions here instructions here instructions here</li>
+//       <li></li>
+//       <li></li>
+//       <li></li>
+//       <li></li>
+//     </ul>`,
+//   },
+//   {
+//     // location: "Main Recipes",
+//     title: "Main Recipe Section Test",
+//     info: "Test Test Test",
+//     ingredients: "&#8226 Test Test",
+//     instructions: `<ul>
+//       <li>instructions here instructions here instructions here</li>
+//       <li></li>
+//       <li></li>
+//       <li></li>
+//       <li></li>
+//     </ul>`,
+//   },
+// ];
+
+// const recipesArr = topRecipeArr.concat(mainRecipeArr);
+
+//*=================================================
 //! Main Recipe Arrows =====================================
+
 let pagination = document.querySelector(".pagination");
 let dots = pagination.children;
 let screenWidth = window.innerWidth;
@@ -416,3 +594,98 @@ nextArrow.addEventListener("click", () => {
 
 // mainContainer.style.overflowY = "scroll";
 // enableScroll();
+
+//! Recipe Array (before split)
+// const recipesArr = [
+//   {
+//     location: "Top Recipes",
+//     title: "Taco Soup",
+//     info: "Talk about taco Soup",
+//     ingredients: "&#8226 Taco Soup <br> &#8226 Taco Soup <br>",
+//     instructions: "instructions here instructions here instructions here",
+//   },
+//   {
+//     location: "Top Recipes",
+//     title: "Carrot Veggie Dish",
+//     info: "Talk about Carrot Veggie Dish",
+//     ingredients: `<ul>
+//         <li>&#8226 Carrot Veggie Dish</li>
+//         <li>&#8226 Carrot Veggie Dish</li>
+//         <li>&#8226 Carrot Veggie Dish</li>
+//         <li>&#8226 Carrot Veggie Dish</li>
+//         <li>&#8226 Carrot Veggie Dish</li>
+//       </ul>`,
+//     instructions: `<ul>
+//       <li>instructions here instructions here instructions here</li>
+//       <li></li>
+//       <li></li>
+//       <li></li>
+//       <li></li>
+//     </ul>`,
+//   },
+//   {
+//     location: "Top Recipes",
+//     title: "Cast Iron Steak",
+//     info: "Talk about cast iron steak",
+//     ingredients: "&#8226 ",
+//     instructions: `<ul>
+//       <li>instructions here instructions here instructions here</li>
+//       <li></li>
+//       <li></li>
+//       <li></li>
+//       <li></li>
+//     </ul>`,
+//   },
+//   {
+//     location: "Top Recipes",
+//     title: "Taco Cups",
+//     info: "Talk about Taco Cups",
+//     ingredients: "&#8226 ",
+//     instructions: `<ul>
+//       <li>instructions here instructions here instructions here</li>
+//       <li></li>
+//       <li></li>
+//       <li></li>
+//       <li></li>
+//     </ul>`,
+//   },
+//   {
+//     location: "Top Recipes",
+//     title: "Pan fried COD",
+//     info: "Talk about Pan Frying COD",
+//     ingredients: "&#8226 Pan fried COD",
+//     instructions: `<ul>
+//       <li>instructions here instructions here instructions here</li>
+//       <li></li>
+//       <li></li>
+//       <li></li>
+//       <li></li>
+//     </ul>`,
+//   },
+//   {
+//     location: "Top Recipes",
+//     title: "Blu+Bri Burgers",
+//     info: "Talk about Blu + Bri Burgers",
+//     ingredients: "&#8226 Blu + Bri Burgers",
+//     instructions: `<ul>
+//       <li>instructions here instructions here instructions here</li>
+//       <li></li>
+//       <li></li>
+//       <li></li>
+//       <li></li>
+//     </ul>`,
+//   },
+//   {
+//     location: "Main Recipes",
+//     title: "Main Recipe Section Test",
+//     info: "Test Test Test",
+//     ingredients: "&#8226 Test Test",
+//     instructions: `<ul>
+//       <li>instructions here instructions here instructions here</li>
+//       <li></li>
+//       <li></li>
+//       <li></li>
+//       <li></li>
+//     </ul>`,
+//   },
+// ];
